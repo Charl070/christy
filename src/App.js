2ec16f315f6d1939441adeefbe0552e39cb2e0e4
Charles Anesu Kombo <62628677+Charl070@users.noreply.github.com>
2023-07-22 03:49:10 +0200

    feat: new

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>My worst fear is to wake up and I can't talk to you again. I wish I will see you again sweetheart. I love you Christine Msiska</h1>
+     <h1> I love you Christine Msiska</h1>
     </div>
   );
 }
